6a35deed12f71e2975f66c981c342f7c9f4e0549
Chessboy71 <48567451+Chessboy71@users.noreply.github.com>
2020-05-07 01:50:58 +0200

    adding the section scrolling effect

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Landing Page Project
 #Udacity nanodegree web dev program
+
+Adding functionalities using the DOM to make a static page more fun ! 
+
+Functionalities to add : 
+    A smooth scroll to the sections from the nav 
+    Making some cool effects while you scroll Around the sections 
+    and making a dynamic navigation bar. 
+
--- js/app.js
+++ js/app.js
@@ -1,52 +1,14 @@
-
-const sectionScroll = (e) => {
-    e.preventDefault();
-      wantedActive = e.target.innerHTML.toLowerCase();
-      let activeSec = document.getElementById(wantedActive);
-      console.log(activeSec.offsetTop);
-      window.scroll({
-          top:activeSec.offsetTop,
-          left:0,
-          behavior:'smooth'
-      })
-      return wantedActive;
-}
-
-window.addEventListener('scroll',{
-
-})
-
-
-
-
-
-
-
-
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
 /**
  * Define Global Variables
  * 
 */
+const options = {
+    threshold:0.8
+}
+let observer = new IntersectionObserver(navCheck, options);
 let wantedActive = 'section1'; 
 const sectionElements = document.querySelectorAll('section');
 const navMenu = document.querySelector('.navbar__menu');
-
-
 /**
  * End Global Variables
  * Start Helper Functions
@@ -60,35 +22,51 @@ navCreate = (e) => {
     list.appendChild(anchor);
     document.getElementById("navbar__list").appendChild(list);
 }
-
-
-
-
+function navCheck(entries){
+    entries.forEach(entry => {
+        sectionElements.forEach(section => {
+            section.classList.remove('activated');
+        })
+        entry.target.classList.add('activated');
+    
+    })
+}
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
+sectionElements.forEach(section => {
+    observer.observe(section)
+})
 
+const sectionScroll = (e) => {
+    e.preventDefault();
+      wantedActive = e.target.innerHTML.toLowerCase();
+      let activeSec = document.getElementById(wantedActive);
+      console.log(activeSec.offsetTop);
+      window.scrollTo({
+          top:activeSec.offsetTop,
+          left:0,
+          behavior:'smooth'
+      })
+      return wantedActive;
+      
+}
 // build the nav
 sectionElements.forEach(element => navCreate(element))
 
 // Add class 'active' to section when near top of viewport
 
 
+
 // Scroll to anchor ID using scrollTO event
-
-
+navMenu.addEventListener('click', sectionScroll);
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
-// Build menu 
-
-// Scroll to section on link click
-navMenu.addEventListener('click', sectionScroll);
 
 // Set sections as active
 
